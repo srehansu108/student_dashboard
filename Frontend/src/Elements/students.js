d08bf8333c730709071266c3a4181a1152d9f3dc
srehansu108 <srehansu.barik@gmail.com>
2024-12-25 14:43:39 +0530

    students.js

--- Frontend/src/Elements/students.js
+++ Frontend/src/Elements/students.js
@@ -10,7 +10,7 @@ const Students = () => {
   // Fetch students from the backend API
   const fetchStudents = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/students', {
+      const response = await fetch('https://student-dashboard-9nn1.onrender.com', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
